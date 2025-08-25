670fb0cff162a57b3e98fdbc4bafd69bbfa6796d
Aden Tranter <me@adentranter.com>
2025-08-26 02:38:42 +1000

    chore: ignore local/build artifacts (.letscode, .claude, dist, node_modules)

--- dist/commands/init.js
+++
@@ -1,7 +0,0 @@
-import { ensureLocalScaffold, ensureGlobalScaffold, repoRoot } from "../lib/paths.js";
-export async function cmdInit() {
-    const root = await repoRoot();
-    await ensureLocalScaffold(root);
-    await ensureGlobalScaffold();
-    console.log("[INFO] Initialised .letscode/ and ~/.letscode/");
-}
